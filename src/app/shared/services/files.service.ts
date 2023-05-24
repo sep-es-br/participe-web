1e26daad3844ea521be481390a5dc713555b80cc
Marcos Antonio dos Santos <marcos.santos@sep.local>
2023-05-24 11:23:12 -0300

    correção hearders upload

--- src/app/shared/services/files.service.ts
+++ src/app/shared/services/files.service.ts
@@ -6,11 +6,10 @@ import Common from '../util/Common';
 @Injectable()
 export class FilesService {
   private url = `${environment.apiEndpoint}/files`;
-  private headers = Common.buildFileUploadHeaders();
 
   constructor(private http: HttpClient) {}
 
   uploadFile(file: FormData) {
-    return this.http.post<any>(`${this.url}/upload`, file, { headers: Common.buildHeaders() }).toPromise();
+    return this.http.post<any>(`${this.url}/upload`, file, { headers: Common.buildFileUploadHeaders() }).toPromise();
   }
 }
