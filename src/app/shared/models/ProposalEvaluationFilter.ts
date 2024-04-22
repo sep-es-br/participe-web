6c4c0eaebf7c550d0edf7822f59b5a14d27dd432
Artur Faria <artur.faria.1992@gmail.com>
2024-04-22 14:41:55 -0300

    feat: adiciona ProposalEvaluationFilter

---
+++ src/app/shared/models/ProposalEvaluationFilter.ts
@@ -0,0 +1,9 @@
+export class ProposalEvaluationFilter {
+  proposalStatus: string = null;
+  microrregion: number = null; //localityId?
+  themeArea: string = null; // possivel number(id)?
+  budgetCategory: string = null; // possivel number(id)?
+  entity: string = null; // possivel number(id)?
+  loaIncluded: boolean = null;
+  text: string = "";
+}
