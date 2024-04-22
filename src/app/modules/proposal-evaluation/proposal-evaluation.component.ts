c6f72348571f5f37b7f8bc1dde9d329224d9a4d6
Artur Faria <artur.faria.1992@gmail.com>
2024-04-22 12:08:59 -0300

    ref: adiciona metodos toggleSearch, getIcon, searchHandle, modelo mock de proposta

--- src/app/modules/proposal-evaluation/proposal-evaluation.component.ts
+++ src/app/modules/proposal-evaluation/proposal-evaluation.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { ActionBarService } from "@app/core/actionbar/app.actionbar.actions.service";
 import { BreadcrumbService } from "@app/core/breadcrumb/breadcrumb.service";
+import { IProposal } from "@app/shared/interface/IProposal";
 import { Conference } from "@app/shared/models/conference";
 import { ConferenceService } from "@app/shared/services/conference.service";
+import { HelperUtils } from "@app/shared/util/HelpersUtil";
 
 @Component({
   selector: "app-proposal-evaluation",
@@ -11,15 +13,71 @@ import { ConferenceService } from "@app/shared/services/conference.service";
   styleUrl: "./proposal-evaluation.component.scss",
 })
 export class ProposalEvaluationComponent implements OnInit {
+  loading: boolean = false;
   conferences: Conference[];
   conferenceSelect: Conference = new Conference();
+  search: boolean = false;
   showSelectConference: boolean = false;
+  proposalList: IProposal[] = [];
 
   constructor(
     private breadcrumbService: BreadcrumbService,
     private actionBarService: ActionBarService,
     private conferenceService: ConferenceService
-  ) {}
+  ) {
+    this.proposalList = [
+      {
+        status: "Avaliado",
+        microrregion: "Caparaó",
+        descriptionText:
+          "Elaborar e implementar um plano abrangente de educação e formação profissional direcionado a indivíduos detidos, visando proporcionar-lhes oportunidades significativas para adquirir novas habilidades, competências e conhecimentos que os habilitem a reintegrar-se de forma eficaz e construtiva na comunidade após o período de encarceramento.",
+        themeArea: "02. Segurança Pública e Justiça",
+        budgetCategory: "Segurança e Cidadania",
+        entities: [
+          "SECULT",
+          "SEAMA",
+          "SEDU",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+          "PGE",
+        ],
+      },
+      {
+        status: "Não Avaliado",
+        microrregion: "Central Serrana",
+        descriptionText:
+          "Propor a inserção de um currículo dedicado à Robótica nos programas educacionais do Estado do Espírito Santo, tanto teórico quanto prático, incluindo laboratórios específicos para a aprendizagem e experimentação nesse campo. Esta iniciativa não apenas ampliaria o leque de disciplinas oferecidas, mas também prepararia os estudantes para as demandas crescentes do mercado de trabalho em áreas relacionadas à tecnologia e inovação.",
+        themeArea: "01. Educação, Cultura, Esporte e Lazer",
+        budgetCategory: "Educação",
+        entities: [
+          "SECULT",
+          "SEAMA",
+          "SEDU",
+          // "PGE",
+          // "PGE",
+          // "PGE",
+          // "PGE",
+          // "PGE",
+          // "PGE",
+        ],
+      },
+    ];
+  }
 
   async ngOnInit() {
     await this.loadConferences();
@@ -44,6 +102,17 @@ export class ProposalEvaluationComponent implements OnInit {
     this.buildBreadcrumb();
     this.showSelectConference = false;
   }
+
+  toggleSearch() {
+    this.search = !this.search;
+    sessionStorage.setItem("searchState", this.search ? "show" : "hide");
+  }
+
+  getIcon(icon?: string) {
+    return HelperUtils.loadingIcon(icon, this.loading);
+  }
+
+  searchHandle() {}
 
   private buildBreadcrumb() {
     this.breadcrumbService.setItems([
