7f6d2b4d1d92912e86f362eb65bc734b615ff055
Artur Faria <artur.faria.1992@gmail.com>
2024-04-22 12:10:01 -0300

    feat: adiciona interface IProposal

---
+++ src/app/shared/interface/IProposal.ts
@@ -0,0 +1,8 @@
+export interface IProposal {
+    status: string;
+    microrregion: string;
+    descriptionText: string;
+    themeArea: string;
+    budgetCategory: string;
+    entities: string[];
+}
