b7708ea6e675923e5ab6f1bda35d5bea054ad87e
Artur Faria <artur.faria.1992@gmail.com>
2024-04-23 09:04:52 -0300

    feat: adiciona componente evaluate

---
+++ src/app/modules/proposal-evaluation/evaluate/evaluate.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-evaluate",
+  standalone: false,
+  templateUrl: "./evaluate.component.html",
+  styleUrl: "./evaluate.component.scss",
+})
+export class EvaluateComponent {}
