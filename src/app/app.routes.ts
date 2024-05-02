922f29f512a35b5ffe2b9d104f1863a7603e50c3
Artur Faria <artur.faria.1992@gmail.com>
2024-05-02 13:56:07 -0300

    feat: adiciona roteamento para o componente EvaluationSectionsComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,6 +17,7 @@ import {CitizenComponent} from './modules/citizen/citizen.component';
 import {ControlPanelDashboardComponent} from './modules/control-panel-dashboard/control-panel-dashboard.component';
 import {ConferenceListComponent} from './modules/conference/conference-list/conference-list.component';
 import { MeetPanelComponent } from './modules/conference/meeting/meet-panel/meet-panel.component';
+import { EvaluationSectionsComponent } from './modules/evaluation-sections/evaluation-sections.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
@@ -31,6 +32,7 @@ export const routes: Routes = [
   {path: 'administration/conferences/conference', component: ConferenceComponent},
   {path: 'administration/conferences/:id/meeting', component: MeetingComponent},
   {path: 'administration/conferences/:id/meeting/:idm/panel', component: MeetPanelComponent},
+  {path: 'administration/evaluation-sections', component: EvaluationSectionsComponent},
   {path: 'administration/citizen', component: CitizenComponent},
   {path: 'moderation/search', component: ModerationComponent},
   {path: 'moderation/moderate/:id/:conferenceId', component: ModerateComponent},
