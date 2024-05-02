c2b092c913dd5bc12f60dd9c9ddb7d500d20d172
Artur Faria <artur.faria.1992@gmail.com>
2024-05-02 17:26:27 -0300

    ref: adiciona metodo getEvaluationSectionsMock por motivos de teste

--- src/app/shared/services/evaluation-sections.service.ts
+++ src/app/shared/services/evaluation-sections.service.ts
@@ -11,7 +11,44 @@ export class EvaluationSectionsService {
 
   private headers: HttpHeaders = Common.buildHeaders();
 
+  private data = [
+    {
+      entity: "SECOM - SUPERINTENDÊNCIA ESTADUAL DE COMUNICAÇÃO SOCIAL",
+      section: "GAF - GRUPO ADMINISTRATIVO FINANCEIRO",
+      server: "CLAUDIO MARCIO NASCIMENTO",
+    },
+    {
+      entity: "SCM - SECRETARIA DA CASA MILITAR",
+      section: "GRUPO FINANCEIRO SETORIAL-GFS-SCM",
+      server: "Todos",
+    },
+    {
+      entity: "SEAG - SECRETARIA DA AGRICULTURA, ABASTECIMENTO, AQUICULTURA",
+      section: "GPO - GRUPO DE PLANEJAMENTO E ORCAMENTO-SEAG",
+      server: "MAX EMANUEL FLORES EVANGELISTA CALDERARO",
+    },
+    {
+      entity: "PGE - PROCURADORIA GERAL DO ESTADO",
+      section: "GPO - GRUPO DE PLANEJAMENTO E ORCAMENTO-PGE",
+      server: "CARLOS HENRIQUE DE ALMEIDA",
+    },
+    {
+      entity: "SEAMA - SECRETARIA DO MEIO AMBIENTE E RECURSOS HÍDRICOS",
+      section: "GPO - GRUPO DE PLANEJAMENTO E ORCAMENTO-SEAMA",
+      server: "HAMILTON PERILO SILVA DE OLIVEIRA",
+    },
+    {
+      entity: "SECONT - SECRETARIA DE CONTROLE E TRANSPARÊNCIA",
+      section: "GPO - GRUPO DE PLANEJAMENTO E ORCAMENTO-SECONT",
+      server: "LORENA DOS SANTOS SOUZA",
+    },
+  ];
+
   constructor(private _http: HttpClient) {}
+
+  public getEvaluationSectionsMock() {
+    return this.data;
+  }
 
   private getEvaluationSections(): Promise<any> {
     return this._http.get(this._url, { headers: this.headers }).toPromise();
