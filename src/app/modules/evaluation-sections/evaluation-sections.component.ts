69d3a56240ecb6763e55e393d71cd578f292ebab
Artur Faria <artur.faria.1992@gmail.com>
2024-05-03 14:56:31 -0300

    feat: adiciona formulario evaluatorsForm, logica de inicializacao e edicao

--- src/app/modules/evaluation-sections/evaluation-sections.component.ts
+++ src/app/modules/evaluation-sections/evaluation-sections.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
+import { AbstractControl, FormControl, FormGroup } from "@angular/forms";
 import { BreadcrumbService } from "@app/core/breadcrumb/breadcrumb.service";
 import { EvaluationSectionsService } from "@app/shared/services/evaluation-sections.service";
 
@@ -12,29 +13,39 @@ export class EvaluationSectionsComponent implements OnInit, OnDestroy {
   search = false;
   showForm = false;
 
+  evaluatorsForm: FormGroup;
+
   data: any[] = [];
+
+  entityList: any[] = [];
+  sectionList: any[] = [];
+  serverList: any[] = [];
 
   constructor(
     private breadcrumbService: BreadcrumbService,
     private evaluationSectionsService: EvaluationSectionsService
   ) {}
 
+  // get formControls(): Array<AbstractControl<any, any>> {
+  //   const keys = Object.keys(this.evaluatorsForm.controls);
+
+  //   return keys.map((k) => this.evaluatorsForm.controls[k]);
+  // }
+
   ngOnInit(): void {
     this.buildBreadcrumb();
     this.getData();
   }
 
-  public getData() {
-    this.data = this.evaluationSectionsService.getEvaluationSectionsMock();
-  }
-
   public showCreateEvaluator(data?: any) {
     console.log("create");
-    if (data) {
-      console.log(data);
-    }
-
     this.showForm = true;
+
+    this.initEvaluatorsForm();
+
+    if (data) {
+      this.prepareEvaluatorsForm(data);
+    }
   }
 
   public showEdit(data: any) {
@@ -47,6 +58,38 @@ export class EvaluationSectionsComponent implements OnInit, OnDestroy {
     console.log(data);
   }
 
+  public initEvaluatorsForm(data?: any) {
+    this.evaluatorsForm = new FormGroup({
+      entity: new FormControl(""),
+      sections: new FormControl([]),
+      servers: new FormControl([]),
+    });
+
+    console.log(this.evaluatorsForm.value);
+
+    this.getEntityList();
+  }
+
+  public entityChanged(event: any) {
+    // fazer GET para endpoint de setores passando event.value como queryParam
+    // ex: localhost:8080/participe/setores?orgao=algumacoisa
+
+    console.log(event.value);
+    this.getSectionList();
+  }
+
+  public sectionsChanged(event: any) {
+    // fazer GET para endpoint de servidores passando event.value como queryParam
+    // ex: localhost:8080/participe/servidores?setor=algumacoisa
+
+    console.log(event.value);
+    this.getServerList();
+  }
+
+  public saveEvaluatorsForm() {
+    console.log(this.evaluatorsForm.value);
+  }
+
   private buildBreadcrumb() {
     this.breadcrumbService.setItems([
       { label: "administration.label" },
@@ -55,6 +98,28 @@ export class EvaluationSectionsComponent implements OnInit, OnDestroy {
         routerLink: ["/administration/evaluation-sections"],
       },
     ]);
+  }
+
+  private getData() {
+    this.data = this.evaluationSectionsService.getEvaluationSectionsMock();
+  }
+
+  private getEntityList() {
+    this.entityList = this.evaluationSectionsService.getEntityListMock();
+  }
+  private getSectionList() {
+    this.sectionList = this.evaluationSectionsService.getSectionListMock();
+  }
+  private getServerList() {
+    this.serverList = this.evaluationSectionsService.getServerListMock();
+  }
+
+  private prepareEvaluatorsForm(data: any) {
+    this.evaluatorsForm.get("entity").patchValue(data.entity);
+    this.evaluatorsForm.get("sections").patchValue(data.section.split(","));
+    this.evaluatorsForm.get("servers").patchValue(data.server.split(","));
+
+    console.log(this.evaluatorsForm.value);
   }
 
   ngOnDestroy(): void {
