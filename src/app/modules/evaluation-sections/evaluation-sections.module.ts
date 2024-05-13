d1660c57d8cb8a031ba3d9ccf7f1ec99fb76661d
Artur Faria <artur.faria.1992@gmail.com>
2024-05-13 17:40:49 -0300

    ref: importa modulos TranslateModule, ComponentsModule

--- src/app/modules/evaluation-sections/evaluation-sections.module.ts
+++ src/app/modules/evaluation-sections/evaluation-sections.module.ts
@@ -4,9 +4,10 @@ import { AppRoutingModule } from "@app/app-routing.module";
 import { CoreModule } from "@app/core/core.module";
 import { TranslateModule } from "@ngx-translate/core";
 import { EvaluationSectionsComponent } from "./evaluation-sections.component";
+import { ComponentsModule } from "@app/shared/components/components.module";
 
 @NgModule({
-  imports: [AppRoutingModule, CommonModule, CoreModule, TranslateModule],
+  imports: [AppRoutingModule, CommonModule, CoreModule, TranslateModule, ComponentsModule],
   declarations: [EvaluationSectionsComponent],
   exports: [EvaluationSectionsComponent],
 })
