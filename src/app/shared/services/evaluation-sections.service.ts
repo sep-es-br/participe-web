489e18f0da585dd51163e4172bc854f0b7beb0e2
Artur Faria <artur.faria.1992@gmail.com>
2024-05-13 17:41:52 -0300

    ref: adiciona listas mockadas com tipagem correta; altera metodos getEvaluationSectionList, postEvaluationSection, putEvaluationSection

--- src/app/shared/services/evaluation-sections.service.ts
+++ src/app/shared/services/evaluation-sections.service.ts
@@ -2,69 +2,92 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "@environments/environment";
 import Common from "../util/Common";
+import { IOrganogramaData } from "../interface/IOrganogramaData";
+import { Observable } from "rxjs";
+import { IEvaluationSection, IEvaluationSectionCreate } from "../interface/IEvaluationSection";
 
 @Injectable({
   providedIn: "root",
 })
 export class EvaluationSectionsService {
-  private _url = `${environment.apiEndpoint}/placeholder`;
+  private _url = `${environment.apiEndpoint}/evaluation-sections`;
 
   private headers: HttpHeaders = Common.buildHeaders();
 
   private data = [
     {
       entity: "SECOM - SUPERINTENDÊNCIA ESTADUAL DE COMUNICAÇÃO SOCIAL",
-      section: "GAF - GRUPO ADMINISTRATIVO FINANCEIRO",
-      server: "CLAUDIO MARCIO NASCIMENTO",
+      sections: "GAF - GRUPO ADMINISTRATIVO FINANCEIRO",
+      servers: "CLAUDIO MARCIO NASCIMENTO",
     },
     {
       entity: "SCM - SECRETARIA DA CASA MILITAR",
-      section: "GRUPO FINANCEIRO SETORIAL-GFS-SCM",
-      server: "Todos",
+      sections: "GRUPO FINANCEIRO SETORIAL-GFS-SCM",
+      servers: "Todos",
     },
     {
       entity: "SEAG - SECRETARIA DA AGRICULTURA, ABASTECIMENTO, AQUICULTURA",
-      section: "GPO - GRUPO DE PLANEJAMENTO E ORCAMENTO-SEAG",
-      server: "MAX EMANUEL FLORES EVANGELISTA CALDERARO",
+      sections: "GPO - GRUPO DE PLANEJAMENTO E ORCAMENTO-SEAG",
+      servers: "MAX EMANUEL FLORES EVANGELISTA CALDERARO",
     },
     {
       entity: "PGE - PROCURADORIA GERAL DO ESTADO",
-      section: "GPO - GRUPO DE PLANEJAMENTO E ORCAMENTO-PGE",
-      server: "CARLOS HENRIQUE DE ALMEIDA",
+      sections: "GPO - GRUPO DE PLANEJAMENTO E ORCAMENTO-PGE",
+      servers: "CARLOS HENRIQUE DE ALMEIDA",
     },
     {
       entity: "SEAMA - SECRETARIA DO MEIO AMBIENTE E RECURSOS HÍDRICOS",
-      section: "GPO - GRUPO DE PLANEJAMENTO E ORCAMENTO-SEAMA",
-      server: "HAMILTON PERILO SILVA DE OLIVEIRA",
+      sections: "GPO - GRUPO DE PLANEJAMENTO E ORCAMENTO-SEAMA",
+      servers: "HAMILTON PERILO SILVA DE OLIVEIRA",
     },
     {
       entity: "SECONT - SECRETARIA DE CONTROLE E TRANSPARÊNCIA",
-      section: "GPO - GRUPO DE PLANEJAMENTO E ORCAMENTO-SECONT",
-      server: "LORENA DOS SANTOS SOUZA",
+      sections: "GPO - GRUPO DE PLANEJAMENTO E ORCAMENTO-SECONT",
+      servers: "LORENA DOS SANTOS SOUZA",
     },
   ];
 
-  private entityList = [
-    "SECOM - Superintendência Estadual de Comunicação Social",
-    "SCM - Secretaria da Casa Militar",
-    "SCV - Secretaria da Casa Civil",
-    "PGE - Procuradoria Geral do Estado",
-    "SEAG - Secretaria da Agricultura, Abastecimento, Aquicultura e Pesca",
-    "SEAMA - Secretaria do Meio Ambiente e Recursos Hídricos",
-  ];
+  // private entityList = [
+  //   "SECOM - Superintendência Estadual de Comunicação Social",
+  //   "SCM - Secretaria da Casa Militar",
+  //   "SCV - Secretaria da Casa Civil",
+  //   "PGE - Procuradoria Geral do Estado",
+  //   "SEAG - Secretaria da Agricultura, Abastecimento, Aquicultura e Pesca",
+  //   "SEAMA - Secretaria do Meio Ambiente e Recursos Hídricos",
+  // ];
 
-  private sectionList = [
-    "ASSESP - Assessoria Especial",
-    "GPO - Grupo de Planejamento e Orçamento",
-    "GETAD - Gerência Técnico Administrativa",
-    "SUBEPP - Subsecretaria de Estado de Planejamento e Projetos",
-  ];
+  // private sectionList = [
+  //   "ASSESP - Assessoria Especial",
+  //   "GPO - Grupo de Planejamento e Orçamento",
+  //   "GETAD - Gerência Técnico Administrativa",
+  //   "SUBEPP - Subsecretaria de Estado de Planejamento e Projetos",
+  // ];
 
-  private serverList = [
-    "CARLOS HENRIQUE DE ALMEIDA ESTAGIARIO JOVENS VALORES - NIVEL SUPERIOR - 20HS - CGTI - SEP - GOVES",
-    "MAX EMANUEL FLORES EVANGELISTA CALDERARO CHEFE GRUPO DE PLANEJAMENTO E ORCAMENTO QCE-05 - GPO - SEP - GOVES",
-    "FABIANA DO ESPIRITO SANTO CARDOSO CHEFE GRUPO RECURSOS HUMANOS QCE-05 - GRH - SEP - GOVES",
-  ];
+  // private serverList = [
+  //   "CARLOS HENRIQUE DE ALMEIDA ESTAGIARIO JOVENS VALORES - NIVEL SUPERIOR - 20HS - CGTI - SEP - GOVES",
+  //   "MAX EMANUEL FLORES EVANGELISTA CALDERARO CHEFE GRUPO DE PLANEJAMENTO E ORCAMENTO QCE-05 - GPO - SEP - GOVES",
+  //   "FABIANA DO ESPIRITO SANTO CARDOSO CHEFE GRUPO RECURSOS HUMANOS QCE-05 - GRH - SEP - GOVES",
+  // ];
+
+  private organizationsMockList: Array<IOrganogramaData> = [
+    {guid: "OrgTesteA", name: "Organização de Teste 1"},
+    {guid: "OrgTesteB", name: "Organização de Teste 2"},
+    {guid: "OrgTesteC", name: "Organização de Teste 3"},
+  ]
+
+  private sectionsMockList: Array<IOrganogramaData> = [
+    {guid: "SectionTesteA", name: "Setor de Teste 1"},
+    {guid: "SectionTesteB", name: "Setor de Teste 2"},
+    {guid: "SectionTesteC", name: "Setor de Teste 3"},
+  ]
+
+  private serversMockList: Array<IOrganogramaData> = [
+    {guid: 'propidByAuthDoRelacionamentoIS_EVALUATED_BY', name: "Artur Uhlig de Faria"},
+    {guid:'guid1', name: 'Bruno Matos Marques Barbosa'},
+    {guid:'guid2', name: 'Luiz Guilherme Zortea Machado'},
+    {guid:'guid3', name: 'Wagner Bertolino da Cruz'},
+    {guid:'guid4', name: 'Diego Gutemberg Gaede'},
+  ]
 
   constructor(private _http: HttpClient) {}
 
@@ -72,31 +95,31 @@ export class EvaluationSectionsService {
     return this.data;
   }
 
-  public getEntityListMock() {
-    return this.entityList;
+  public getOrganizationsMockList() {
+    return this.organizationsMockList;
   }
 
-  public getSectionListMock() {
-    return this.sectionList;
+  public getSectionsMockList() {
+    return this.sectionsMockList;
   }
 
-  public getServerListMock() {
-    return this.serverList;
+  public getServersMockList() {
+    return this.serversMockList;
   }
 
-  private getEvaluationSections(): Promise<any> {
-    return this._http.get(this._url, { headers: this.headers }).toPromise();
+  public getEvaluationSectionsList(): Promise<Array<IEvaluationSection>> {
+    return this._http.get<Array<IEvaluationSection>>(this._url, { headers: this.headers }).toPromise();
   }
 
-  private postEvaluationSection(body: any): Promise<any> {
+  public postEvaluationSection(body: IEvaluationSectionCreate): Promise<IEvaluationSection> {
     return this._http
-      .post(this._url, body, { headers: this.headers })
+      .post<IEvaluationSection>(this._url, body, { headers: this.headers })
       .toPromise();
   }
 
-  private putEvaluationSection(id: number, body: any): Promise<any> {
+  public putEvaluationSection(id: number, body: IEvaluationSectionCreate): Promise<IEvaluationSection> {
     return this._http
-      .put(`${this._url}/${id}`, body, { headers: this.headers })
+      .put<IEvaluationSection>(`${this._url}/${id}`, body, { headers: this.headers })
       .toPromise();
   }
 }
