08322db647c94dd3ae9ca76fa6500990680afe86
Artur Faria <artur.faria.1992@gmail.com>
2024-05-14 13:12:05 -0300

    ref: altera tipo de retorno dos metodos get, post, put e delete de EvaluationSectionsService

--- src/app/shared/services/evaluation-sections.service.ts
+++ src/app/shared/services/evaluation-sections.service.ts
@@ -4,13 +4,18 @@ import { environment } from "@environments/environment";
 import Common from "../util/Common";
 import { IOrganogramaData } from "../interface/IOrganogramaData";
 import { Observable } from "rxjs";
-import { IEvaluationSection, IEvaluationSectionCreate } from "../interface/IEvaluationSection";
+import {
+  IEvaluationSection,
+  IEvaluationSectionCreate,
+} from "../interface/IEvaluationSection";
 
 @Injectable({
   providedIn: "root",
 })
 export class EvaluationSectionsService {
   private _url = `${environment.apiEndpoint}/evaluation-sections`;
+  private _urlApiOrganograma = "";
+  private _urlApiAcessoCidadao = "";
 
   private headers: HttpHeaders = Common.buildHeaders();
 
@@ -70,24 +75,27 @@ export class EvaluationSectionsService {
   // ];
 
   private organizationsMockList: Array<IOrganogramaData> = [
-    {guid: "OrgTesteA", name: "Organização de Teste 1"},
-    {guid: "OrgTesteB", name: "Organização de Teste 2"},
-    {guid: "OrgTesteC", name: "Organização de Teste 3"},
-  ]
+    { guid: "OrgTesteA", name: "Organização de Teste 1" },
+    { guid: "OrgTesteB", name: "Organização de Teste 2" },
+    { guid: "OrgTesteC", name: "Organização de Teste 3" },
+  ];
 
   private sectionsMockList: Array<IOrganogramaData> = [
-    {guid: "SectionTesteA", name: "Setor de Teste 1"},
-    {guid: "SectionTesteB", name: "Setor de Teste 2"},
-    {guid: "SectionTesteC", name: "Setor de Teste 3"},
-  ]
+    { guid: "SectionTesteA", name: "Setor de Teste 1" },
+    { guid: "SectionTesteB", name: "Setor de Teste 2" },
+    { guid: "SectionTesteC", name: "Setor de Teste 3" },
+  ];
 
   private serversMockList: Array<IOrganogramaData> = [
-    {guid: 'propidByAuthDoRelacionamentoIS_EVALUATED_BY', name: "Artur Uhlig de Faria"},
-    {guid:'guid1', name: 'Bruno Matos Marques Barbosa'},
-    {guid:'guid2', name: 'Luiz Guilherme Zortea Machado'},
-    {guid:'guid3', name: 'Wagner Bertolino da Cruz'},
-    {guid:'guid4', name: 'Diego Gutemberg Gaede'},
-  ]
+    {
+      guid: "propidByAuthDoRelacionamentoIS_EVALUATED_BY",
+      name: "Artur Uhlig de Faria",
+    },
+    { guid: "guid1", name: "Bruno Matos Marques Barbosa" },
+    { guid: "guid2", name: "Luiz Guilherme Zortea Machado" },
+    { guid: "guid3", name: "Wagner Bertolino da Cruz" },
+    { guid: "guid4", name: "Diego Gutemberg Gaede" },
+  ];
 
   constructor(private _http: HttpClient) {}
 
@@ -108,18 +116,61 @@ export class EvaluationSectionsService {
   }
 
   public getEvaluationSectionsList(): Promise<Array<IEvaluationSection>> {
-    return this._http.get<Array<IEvaluationSection>>(this._url, { headers: this.headers }).toPromise();
-  }
-
-  public postEvaluationSection(body: IEvaluationSectionCreate): Promise<IEvaluationSection> {
     return this._http
-      .post<IEvaluationSection>(this._url, body, { headers: this.headers })
+      .get<Array<IEvaluationSection>>(this._url, {
+        headers: this.headers,
+      })
       .toPromise();
   }
 
-  public putEvaluationSection(id: number, body: IEvaluationSectionCreate): Promise<IEvaluationSection> {
+  public postEvaluationSection(
+    body: IEvaluationSectionCreate
+  ): Promise<IEvaluationSection> {
     return this._http
-      .put<IEvaluationSection>(`${this._url}/${id}`, body, { headers: this.headers })
+      .post<IEvaluationSection>(this._url, body, {
+        headers: this.headers,
+      })
       .toPromise();
   }
+
+  public putEvaluationSection(
+    id: number,
+    body: IEvaluationSectionCreate
+  ): Promise<IEvaluationSection> {
+    return this._http
+      .put<IEvaluationSection>(`${this._url}/${id}`, body, {
+        headers: this.headers,
+      })
+      .toPromise();
+  }
+
+  public deleteEvaluationSection(id: number): Promise<string> {
+    return this._http
+      .delete<string>(`${this._url}/${id}`, { headers: this.headers })
+      .toPromise();
+  }
+
+  /*
+    - Entender melhor os endpoints
+    - Entender os queryParams da url
+  */
+
+  // private getOrganizationsList(): Promise<Array<IOrganogramaData>> {
+  //   return this._http
+  //     .get<Array<IOrganogramaData>>(this._urlApiOrganograma, {
+  //       headers: this.headers,
+  //     })
+  //     .toPromise();
+  // }
+
+  // private getSectionsList(
+  //   organizationGuid: string
+  // ): Promise<Array<IOrganogramaData>> {
+  //   return this._http
+  //     .get<Array<IOrganogramaData>>(this._urlApiOrganograma, {
+  //       headers: this.headers,
+  //       params: { organizationGuid: organizationGuid },
+  //     })
+  //     .toPromise();
+  // }
 }
