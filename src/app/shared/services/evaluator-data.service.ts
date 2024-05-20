329c97130580eb634dc76b6a97cc237addb88af8
Artur Faria <artur.faria.1992@gmail.com>
2024-05-20 17:22:42 -0300

    ref: adiciona argumento orgGuid ao metodo getSectionsList e unitGuid ao metodo getServersList

--- src/app/shared/services/evaluator-data.service.ts
+++ src/app/shared/services/evaluator-data.service.ts
@@ -20,15 +20,15 @@ export class EvaluatorDataService {
       .toPromise();
   }
 
-  public getSectionsList(): Promise<Array<IEvaluatorSection>> {
+  public getSectionsList(orgGuid: string): Promise<Array<IEvaluatorSection>> {
     return this._http
-      .get<Array<IEvaluatorSection>>(`${this._url}/sections`, { headers: this.headers })
+      .get<Array<IEvaluatorSection>>(`${this._url}/sections/${orgGuid}`, { headers: this.headers })
       .toPromise();
   }
 
-  public getServersList(): Promise<Array<IEvaluatorServer>> {
+  public getServersList(unitGuid: string): Promise<Array<IEvaluatorServer>> {
     return this._http
-      .get<Array<IEvaluatorServer>>(`${this._url}/servers`, { headers: this.headers })
+      .get<Array<IEvaluatorServer>>(`${this._url}/servers/${unitGuid}`, { headers: this.headers })
       .toPromise();
   }
 }
