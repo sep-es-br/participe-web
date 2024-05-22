b279ba2e057f79762b56fe5ad87c8341baf19b0f
Artur Faria <artur.faria.1992@gmail.com>
2024-05-22 17:06:56 -0300

    ref: importa pipe TabledataTruncatePipe

--- src/app/modules/evaluators/evaluators.module.ts
+++ src/app/modules/evaluators/evaluators.module.ts
@@ -5,9 +5,10 @@ import { CoreModule } from "@app/core/core.module";
 import { TranslateModule } from "@ngx-translate/core";
 import { EvaluatorsComponent } from "./evaluators.component";
 import { ComponentsModule } from "@app/shared/components/components.module";
+import { TabledataTruncatePipe } from "@app/shared/pipes/tabledata-truncate.pipe";
 
 @NgModule({
-  imports: [AppRoutingModule, CommonModule, CoreModule, TranslateModule, ComponentsModule],
+  imports: [AppRoutingModule, CommonModule, CoreModule, TranslateModule, ComponentsModule, TabledataTruncatePipe],
   declarations: [EvaluatorsComponent],
   exports: [EvaluatorsComponent],
 })
