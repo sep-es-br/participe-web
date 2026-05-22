d7a8bcf648ec97e725fe87524ccff7c3788661ff
geanCarneiro <geancarneiro1@gmail.com>
2026-05-22 12:25:20 -0300

    avanço

--- src/app/modules/conference/meeting/meeting.component.ts
+++ src/app/modules/conference/meeting/meeting.component.ts
@@ -733,8 +733,15 @@ export class MeetingComponent implements OnInit, OnDestroy {
     this.meetingDataSelected = meeting;
   }
 
-  async generateLinkMeetingCredenciamento(meeting: Meeting, isTeam = false){
-    this.meetingUrl = { url : `${this.document.location.origin.replace('4000', '4200')}/${this.location.prepareExternalUrl(`/authority-credential/${meeting.id}${isTeam ? '?isTeam=true' : ''}`)}`};
+  async generateLinkMeetingCredenciamento(meeting: Meeting){
+    this.meetingUrl = { url : `${this.document.location.origin.replace('4000', '4200')}/${this.location.prepareExternalUrl(`/authority-credential/${meeting.id}`)}`};
+    this.modalData = {title: meeting.name};
+    this.ModalService.open('geraLink');
+    this.meetingDataSelected = meeting;
+  }
+
+  async generateLinkTeamCredenciamento(meeting: Meeting){
+    this.meetingUrl = { url : `${this.document.location.origin.replace('4000', '4200')}/${this.location.prepareExternalUrl(`/team-credential/${meeting.id}`)}`};
     this.modalData = {title: meeting.name};
     this.ModalService.open('geraLink');
     this.meetingDataSelected = meeting;
