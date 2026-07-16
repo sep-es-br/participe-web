92ba37c8172b97e6a8a6921af7a1f1b11db41430
geanCarneiro <geancarneiro1@gmail.com>
2026-07-16 14:39:44 -0300

    fix geral

--- src/app/modules/attendance/new-authority/new-authority.component.ts
+++ src/app/modules/attendance/new-authority/new-authority.component.ts
@@ -214,7 +214,7 @@ export class NewAuthorityComponent extends AttendanceModel implements OnInit, On
     }
   }
 
-  handleEnterName(event: any, inName: AutoComplete, inRole: HTMLInputElement) {
+  handleEnterName(event: any, inName: AutoComplete, inRole: HTMLInputElement, inSalvar: HTMLButtonElement) {
     const suggestions = this.filteredNames();
 
     if (suggestions && suggestions.length > 0) {
@@ -224,7 +224,7 @@ export class NewAuthorityComponent extends AttendanceModel implements OnInit, On
       this.form.patchValue({ name: firstItem });
 
       // Dispara a lógica de carga
-      this.applyValue(firstItem);
+      this.applyValue(firstItem, inSalvar);
 
       // Opcional: esconde o painel de sugestões
       inName.hide();
@@ -289,6 +289,9 @@ export class NewAuthorityComponent extends AttendanceModel implements OnInit, On
     this.selectedName = undefined;
     this.idPrecredential = undefined;
     this.form.controls.role.patchValue(undefined);
+    this.form.controls.name.patchValue(undefined);
+    this.form.controls.name.markAsPristine();
+    this.form.controls.name.markAsUntouched();
     this.inOrganizacao.inputEL.nativeElement.focus();
   }
 
